f4bcf2515d29a63db978ae76aa0b1a88c2469b18
tejasvmaske-lab <tejas.v.maske@gmail.com>
2026-03-29 14:25:41 +0530

    Revert "db changes 3"
    This reverts commit 2d74f1289c0ff3561c5a8e25b313f8ae1b5e86af.

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ db_config = {
     'password': os.getenv('DB_PASSWORD', 'Urus@0657'),
     'database': os.getenv('DB_NAME', 'inventory_management'),
     'port': int(os.getenv('DB_PORT', 3306)),
+    'ssl_disabled': False,
     'autocommit': True
 }
 
